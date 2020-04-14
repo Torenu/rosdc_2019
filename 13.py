de1b094284de3dffaa20aba8f71a9b305cdd2aa3
Фёдор Борисов <35196346+Torenu@users.noreply.github.com>
2020-04-14 11:20:03 +0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,9 +1,22 @@
 import cv2 as cv
 from func import *
 
-image = cv.imread("home_pi_imaaage.jpg")
-cv.imshow("im", image)
+SIZE = (400, 300)
 
-binarize(image, d=True)
+RECT = np.float32([[0, 299],
+                   [399, 299],
+                   [399, 0],
+                   [0, 0]])
 
-cv.waitKey(0)
+TRAP = np.float32([[0, 299],
+                   [399, 299],
+                   [320, 200],
+                   [80, 200]])
+
+image = cv.VideoCapture(1)
+while(cv.waitKey(1) != 32):
+    a, cap = image.read()
+    cv.imshow("im", cap)
+    cv.imshow("binary", trans_perspective(binarize(cap), TRAP, RECT, SIZE))
+
+cv.destroyAllWindows()
